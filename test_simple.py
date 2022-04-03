3a0c961633e4dad15b93d1f899a1a0a208e201e4
noahjminer <noahjminer@gmail.com>
2022-04-03 15:49:32 -0500

    added frame depth generation

--- test_simple.py
+++ test_simple.py
@@ -55,7 +55,85 @@ def parse_args():
     return parser.parse_args()
 
 
-def create_depth_image(args, file):
+def create_depth_image_from_frame(args, frame, image_path):
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+
+    download_model_if_doesnt_exist(args.model_name)
+    model_path = os.path.join("models", args.model_name)
+    print("-> Loading model from ", model_path)
+    encoder_path = os.path.join(model_path, "encoder.pth")
+    depth_decoder_path = os.path.join(model_path, "depth.pth")
+
+    # LOADING PRETRAINED MODEL
+    print("   Loading pretrained encoder")
+    encoder = networks.ResnetEncoder(18, False)
+    loaded_dict_enc = torch.load(encoder_path, map_location=device)
+
+    # extract the height and width of image that this model was trained with
+    feed_height = loaded_dict_enc['height']
+    feed_width = loaded_dict_enc['width']
+    filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
+    encoder.load_state_dict(filtered_dict_enc)
+    encoder.to(device)
+    encoder.eval()
+
+    print("   Loading pretrained decoder")
+    depth_decoder = networks.DepthDecoder(
+        num_ch_enc=encoder.num_ch_enc, scales=range(4))
+
+    loaded_dict = torch.load(depth_decoder_path, map_location=device)
+    depth_decoder.load_state_dict(loaded_dict)
+
+    depth_decoder.to(device)
+    depth_decoder.eval()
+
+    # PREDICTING ON EACH IMAGE IN TURN
+    with torch.no_grad():
+        # Load image and preprocess
+        input_image = pil.Image.fromarray(frame)
+        original_width, original_height = input_image.size
+        input_image = input_image.resize((feed_width, feed_height), pil.LANCZOS)
+        input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+
+        # PREDICTION
+        input_image = input_image.to(device)
+        features = encoder(input_image)
+        outputs = depth_decoder(features)
+
+        disp = outputs[("disp", 0)]
+        disp_resized = torch.nn.functional.interpolate(
+            disp, (original_height, original_width), mode="bilinear", align_corners=False)
+
+        # Saving numpy file
+        output_name = os.path.splitext(os.path.basename(image_path))[0]
+        scaled_disp, depth = disp_to_depth(disp, 0.1, 100)
+        # if args.pred_metric_depth:
+        name_dest_npy = os.path.join(image_path, "{}_depth.npy".format(output_name))
+        metric_depth = STEREO_SCALE_FACTOR * depth.cpu().numpy()
+        np.save(name_dest_npy, metric_depth)
+        # else:
+        #     name_dest_npy = os.path.join(output_directory, "{}_disp.npy".format(output_name))
+        #     np.save(name_dest_npy, scaled_disp.cpu().numpy())
+
+        # Saving colormapped depth image
+        disp_resized_np = disp_resized.squeeze().cpu().numpy()
+        vmax = np.percentile(disp_resized_np, 75)
+        normalizer = mpl.colors.Normalize(vmin=disp_resized_np.min(), vmax=vmax)
+        mapper = cm.ScalarMappable(norm=normalizer, cmap='gist_gray')
+        colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
+        im = pil.fromarray(colormapped_im)
+
+        name_dest_im = os.path.join(image_path, "{}_disp.jpeg".format(output_name))
+        im.save(name_dest_im)
+
+        print("   - {}".format(name_dest_im))
+        print("   - {}".format(name_dest_npy))
+
+    return name_dest_im
+
+
+def create_depth_image_from_file(args, file):
     if torch.cuda.is_available():
         device = torch.device("cuda")
 
